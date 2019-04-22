330dfe99162cb9404d5eb98a98a3687d0b68f20d
reckyas <reckyas243@gmail.com>
2019-04-22 14:32:32 +0700

    belajar vue component

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,19 +1,22 @@
-new Vue({
-    el: '#vue-app',
-    data: {
-        healt: 100,
-        ended: false,
-    },
-    methods: {
-        punch: function () {
-            this.healt -= 10;
-            if (this.healt <= 0) {
-                this.ended = true;
-            }
-        },
-        restart: function () {
-            this.healt = 100;
-            this.ended = false;
+Vue.component('greeting', {
+    template: `
+        <strong>My name is {{ name }} <button @click="changeName">Change Name</button></strong>
+    `,
+    data: function () {
+        return {
+            name: 'Mario'
         }
     },
+    methods: {
+        changeName: function () {
+            this.name = 'Yhosi'
+        }
+    },
+})
+var one = new Vue({
+    el: '#vue-app-one'
+})
+
+var two = new Vue({
+    el: '#vue-app-two'
 })
